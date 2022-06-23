99b91fde4added8f3509b355c9402bfb04c8d1d0
smj-edison <stephen.mason.jones@gmail.com>
2022-06-22 17:33:08 -0700

    Add CAN description

--- src/can-protocol.h
+++ src/can-protocol.h
@@ -1,4 +1,12 @@
 #pragma once
 
+// my CAN network implementation is pretty simple:
+// LSB bits 1-5 are the "to" address
+// LSB bits 6-10 are the "from" address
+// LSB bit 11 is a flag, if true it is a broadcast to everyone
+
+// address reassign, 1 byte, the new address
+#define ADDRESS_REASSIGN 0x00
+
 #define NOTE_ON_MSG 0x20
 #define NOTE_OFF_MSG 0x21
